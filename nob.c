8d6f18b0d26f120df50f58e5c822e2743e86afe0
Ivan Gordillo <gordillosolis@icloud.com>
2026-01-28 21:43:03 -0600

    add: nob for my c proyect, ty zizoding

---
+++ nob.c
@@ -0,0 +1,22 @@
+#define NOB_IMPLEMENTATION
+#include "nob.h"
+
+#define BUILD_FOLDER "build/"
+#define SRC_FOLDER "src/"
+
+int main(int argc, char **argv) {
+    NOB_GO_REBUILD_URSELF(argc, argv);
+
+    if (!nob_mkdir_if_not_exists(BUILD_FOLDER))
+        return 1;
+    Nob_Cmd cmd = {0};
+
+    // On POSIX
+    nob_cmd_append(&cmd, "cc", "-Wall", "-Wextra", "-o",
+                   BUILD_FOLDER "automata", SRC_FOLDER "main.c");
+
+    if (!nob_cmd_run(&cmd))
+        return 1;
+
+    return 0;
+}
