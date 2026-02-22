4a16591dc77a0b4de181f440ac21ee072d231737
Ivan Javier Gordillo Solis <gordillosolis@icloud.com>
2026-02-22 00:15:33 +0000

    Add log analysis function and sample log file for testing

---
+++ tarea-3/Parte1_Regex/ejercicio_2.py
@@ -0,0 +1,59 @@
+import re
+from collections import Counter
+
+def analizar_log(archivo):
+    '''
+    Analiza archivo de log y extrae informacin relevante
+    '''
+    with open(archivo, "r", encoding="utf-8") as f:
+        contenido = f.read()
+
+    # a) Extraer errores
+    patron_errores = (
+        r'(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})'    # Timestamp
+        r'.*\[ERROR\]'                              # Etiqueta [ERROR]
+        r'\s*(.*)'                                  # Mensaje de error
+    )
+    errores = re.findall(patron_errores, contenido)
+
+    # b) Extraer direcciones IP
+    patron_ip = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+    ips = re.findall(patron_ip, contenido)
+
+    # c) Extraer archivos
+    patron_archivos = r'(/[\w\/.]+)'
+    archivos = re.findall(patron_archivos, contenido)
+
+    # d) Contar por tipo
+    patron_tipo = r'\[(ERROR|INFO|WARNING)\]'
+    tipos = re.findall(patron_tipo, contenido)
+    contador = Counter(tipos)
+
+    # e) Extaer tiempos
+    patron_tiempo = r'(\d+ms)'
+    tiempos = re.findall(patron_tiempo, contenido)
+
+    # Mostrar resultados
+    print("=== Análisis de sistema.log ===")
+    print("ERRORES encontrados:")
+    for timestamp, mensaje in errores:
+        print(f"\t[{timestamp}] {mensaje}", )
+    
+    print("IPs detectadas:")
+    for ip in ips:
+        print(f"\t{ip}")
+    
+    print("Archivos mencionados:")
+    for archivo in archivos:
+        print(f'\t{archivo}')
+    
+    print("Resumen por tipo:")
+    for tipo in ['ERROR', 'INFO', 'WARNING']:
+        print(f"\t{tipo}: {contador[tipo]}")
+    
+    print("Tiempos de ejecución:")
+    for tiempo in tiempos:
+        print(f"\t{tiempo}")
+
+if __name__ == "__main__":
+    analizar_log("./pruebas/sistema.log")
